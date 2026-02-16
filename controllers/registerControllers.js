06a0f23fec2c4a08cca87820c7bbcf2cdbf10963
Premanshuankit <ankit.premanshu@gmail.com>
2026-02-16 14:33:46 +0530

    added roles in registerControllers

--- controllers/registerControllers.js
+++ controllers/registerControllers.js
@@ -8,14 +8,14 @@ const usersDB = {
 const fsPromises = require('fs/promises')
 const path = require('path')
 const bcrypt = require('bcrypt')
+const ROLES_LIST = require('../config/roles_list')
 
 const handleNewUser = async (req, res) => {
-    const {user, pwd} = req.body
+    const {user, pwd, roles} = req.body
 
     if (!user || !pwd) {
         return res.status(400).send('username and pwd are required')
     }
-
     const duplicate = usersDB.users.find((person) => person.username === user)
     if (duplicate) {
         return res.status(409).send('username already exist, please try with another username')
@@ -23,12 +23,31 @@ const handleNewUser = async (req, res) => {
 
     try {
         const hashedPwd = await bcrypt.hash(pwd, 10)
-        const newUser = { "username": user, "password": hashedPwd }
+
+        let assignedRoles = {}
+
+        if (roles && Array.isArray(roles)) {
+            roles.forEach(role => {
+                if (ROLES_LIST[role]) {
+                    assignedRoles[role] = ROLES_LIST[role]
+                }
+            })
+        }
+
+        if (Object.keys(assignedRoles).length === 0) {
+            assignedRoles = { User: ROLES_LIST.User }
+        }
+
+        const newUser = {
+            username: user,
+            roles: assignedRoles,
+            password: hashedPwd
+        }
         usersDB.setUsers([...usersDB.users, newUser])
+
         await fsPromises.writeFile(
             path.join(__dirname, '..', 'model', 'users.json'),
             JSON.stringify(usersDB.users, null, 2)
-
         )
         console.log(usersDB.users)
         res.status(201).send(`user with name '${user}' was created!!!`)
